f461b5876ede1c7e61abac2dd8a8cdf6cb27550b
FelipeDevjs <sr.felipe.viana@gmail.com>
2022-05-17 16:34:27 -0300

    adicionei popup no erro de login

---
+++ popup.php
@@ -0,0 +1,5 @@
+<div id="Modal" style="display:none;"
+    class="modal bg-gray-100 w-60 absolute top-2.5 right-5 flex items-center rounded-md">
+    <p class="p-5">Mensagem do Model</p>
+    <spam class="bg-red-500 h-1 w-60 absolute bottom-0 rounded-b-md"></spam>
+</div>
